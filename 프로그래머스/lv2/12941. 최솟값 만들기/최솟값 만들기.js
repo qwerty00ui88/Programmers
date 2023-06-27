b52326a20cf6693c80936eb392a510f66ed01752
hameee <113837605+qwerty00ui88@users.noreply.github.com>
2023-06-27 14:21:44 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.83 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -2,6 +2,5 @@ function solution(A,B){
     A.sort((a, b) => a- b)
     B.sort((a, b) => a- b)
     const lastIdx = A.length - 1
-    const sum = A.reduce((acc,_,idx) => [acc[0] + A[idx] * B[lastIdx - idx], acc[1] + A[lastIdx - idx] * B[idx]], [0, 0])
-    return Math.min(...sum)
+    return A.reduce((acc,_,idx) => acc + A[idx] * B[lastIdx - idx], 0)
 }
