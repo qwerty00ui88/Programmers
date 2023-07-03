b355eebdafe3abd7900d1d7a293adbf1c37fa391
hameee <113837605+qwerty00ui88@users.noreply.github.com>
2023-07-03 23:53:53 +0900

    [level 2] Title: 피보나치 수, Time: 6.68 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    const memo = [0, 1]
+    if(n <= 1) return memo[n]
+    for(let i = 2; i <= n; i++) {
+        memo[i] = (memo[i - 1] + memo[i - 2]) % 1234567
+    }
+    return memo[n]
+}
