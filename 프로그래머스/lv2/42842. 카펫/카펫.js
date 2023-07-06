559d521903177d779723d833667a5357a9f9908e
hameee <113837605+qwerty00ui88@users.noreply.github.com>
2023-07-07 05:35:26 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,4 +1,4 @@
 function solution(brown, yellow) {
-    var answer = [];
-    return answer;
+    const sum = (c) => ((brown + yellow) / c) + c === brown / 2 + 2
+    for(let i = 1; i <= Math.sqrt(brown + yellow); i++) if(sum(i)) return [(brown + yellow) / i, i]
 }
