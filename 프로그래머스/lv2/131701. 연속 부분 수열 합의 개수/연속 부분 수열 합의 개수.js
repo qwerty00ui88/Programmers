a1b9c23f7494c1deeaa2bfa11900083b4eff6608
hameee <113837605+qwerty00ui88@users.noreply.github.com>
2023-07-22 00:05:32 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 89.88 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,11 +1,11 @@
 function solution(elements) {
     const double = elements.concat(elements)
-    const result = []
-    const pushNum = (level, idx, sum) => {
+    const result = new Set()
+    const addSum = (level, idx, sum) => {
         if(level > elements.length) return
-        result.push(sum)
-        pushNum(level + 1, idx + 1, sum + double[idx + 1])
+        result.add(sum)
+        addSum(level + 1, idx + 1, sum + double[idx + 1])
     }
-    for(let i = 0; i < elements.length; i++) pushNum(1, i, elements[i])
-    return Array.from(new Set(result)).length
+    for(let i = 0; i < elements.length; i++) addSum(1, i, elements[i])
+    return result.size
 }
