dea761e19cd3b64aafff9a3834ff18d338cdbb33
hameee <113837605+qwerty00ui88@users.noreply.github.com>
2023-09-04 18:52:53 +0900

    [unrated] Title: 뒤에 있는 큰 수 찾기, Time: 97.21 ms, Memory: 149 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    const result = new Array(numbers.length).fill(-1)
+    const stack = []
+    for(let i = 0; i < numbers.length; i++){
+        while(stack && numbers[stack.at(-1)] < numbers[i]){
+            result[stack.pop()] = numbers[i]
+        }
+        stack.push(i)
+    }
+    return result
+}
